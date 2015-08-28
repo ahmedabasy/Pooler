0af8e35b5874f3de19d9a296a497c3d7366fd521
ahmed abasy <ahmed.abasy92@gmail.com>
2015-08-28 23:51:02 +0200

    after putting the mysql2 adapter

--- app/models/carpooling.rb
+++ app/models/carpooling.rb
@@ -2,8 +2,6 @@ class Carpooling < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :location_selection, :destination_location, presence: true
-	# validates :travel_date, : {message: "choose the valid date"}
-	# validates_with ValidateLocation, fields: [:location_selection, :destination_location, :travel_date]
 
 	validate :mydate_is_date?
 
@@ -12,4 +10,6 @@ class Carpooling < ActiveRecord::Base
       errors.add(:travel_date, 'must be a valid date') 
     end
 end
+
+
 end
